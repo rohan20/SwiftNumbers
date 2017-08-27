d825a719ce27595c3d750332035975f548c28c2c
rohan20 <rohan.bvcoe@gmail.com>
2017-08-28 02:09:03 +0530

    Basic operations

--- Contents.swift
+++ Contents.swift
@@ -3,3 +3,44 @@
 import UIKit
 
 var str = "Hello, playground"
+
+//type inference
+var age = 30
+
+//explicitly declared type
+var weight: Int = 200
+
+var someNum: Double = 2312312312312312312
+
+var milesRan = 23.43
+var pi: Float = 3.14
+
+//Arithematic operations
+var area = 15 * 20
+var sum = 12  + 3
+var diff = 12 - 4
+var div = 12 / 2
+var div1 = 13 / 5
+var rem = 13 % 5
+
+var result = "13 divided by 5 = \(div1) with remainder \(rem)";
+
+var randomNumber = 12
+
+if randomNumber % 2 == 0 {
+    print("Even number")
+}else{
+    print("Odd number")
+}
+
+
+
+
+
+
+
+
+
+
+
+
